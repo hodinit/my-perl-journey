580202dc13160a1b3d182f6b7fab4e5b5517da73
hodinit <horatiulazar2003@gmail.com>
2024-04-17 19:05:59 +0300

    day 4

---
+++ my_variables.pl
@@ -0,0 +1,54 @@
+use strict;
+use warnings;
+use diagnostics;
+
+
+my $answer = 42;
+{
+    my $answer = 'forty-two';
+    print "$answer\n";
+}
+print "$answer\n\n";
+
+
+my @numbers = ( 1, 2, 3, 4, 5, 99);
+for my $number (@numbers) {
+my $reciprocal = 1 / $number;
+print "The reciprocal of $number is $reciprocal\n";
+}
+#print $number;
+#print $reciprocal;
+
+
+#package variables
+package main;
+$answer = 42;
+print "$answer\n";
+
+
+package MyCompany::Stuff;
+%MyCompany::Stuff::department_number_for = (
+    finance=> 13,
+    programming => 2,
+    janitorial => 17,
+    executive=> 0,
+);
+for my $i (keys %MyCompany::Stuff::department_number_for){
+    print $i,"\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
