0a90081ea617f5cb0f6e9e217bded02bc1962d16
hodinit <horatiulazar2003@gmail.com>
2024-04-26 18:35:27 +0300

    day 6

---
+++ 17_chapter_4_builtins.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics; 
+
+my $name = "Ovid\n";
+my $last = chomp $name;
+
+
+print $name;
+print $last,"\n";
+
+my $name2 = chr(79).chr(118).chr(105).chr(100);
+print $name2,"\n";
+
+my @values = ( ord('H'), ord('o'), ord('r'), ord('a'), ord('t'),ord('i'),ord('u') );
+print "@values ","\n";
+
+my $name3= chr(72).chr(111).chr(114).chr(97).chr(116).chr(105).chr(117);
+print $name3,"\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
